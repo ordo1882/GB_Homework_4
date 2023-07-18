9f3b0f3cf66392d71fb88b91100ecd3c4099c3b3
Oleg <ordo1882@gmail.com>
2023-07-18 23:31:44 +0200

    Added Tasks 2 and 3

---
+++ Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Enter the number:");
+string number = Console.ReadLine();
+
+bool IsValidateNumber(string num, int position) // Реализация функции проверки наличия ненужных символов и негативных чисел
+{
+    if (!Char.IsDigit(num, position))
+    {
+        Console.WriteLine("Only digits or positive number are allowed.");
+        return false;
+    }
+    return true;
+}
+
+int GetSumDigits() // Реализация функции суммирования цифр
+{
+    int sum = 0;
+
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (!IsValidateNumber(number, i)) // Вызов функции проверки наличия ненужных символов и негативных чисел
+        {
+            return 0; // В случае провала программа останавливается
+        }
+        else
+        {
+            int digit = (int)Char.GetNumericValue(number[i]); // Преобразование цифры из char в int 
+            sum += digit;
+        }
+    }
+
+    return sum;
+}
+
+Console.WriteLine(GetSumDigits());

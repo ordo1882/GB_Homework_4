650577ecb36503bd7528ebe71ffb14d33de5cbc7
Oleg <ordo1882@gmail.com>
2023-07-19 00:12:05 +0200

    Added Tasks 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Enter the first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool IsValidateNumber(int num) // Реализация функции проверки наличия негативного числа
+{
+    if (num > 0)
+    {
+        return true;
+    }
+    else
+    {
+        Console.WriteLine("Need to be a positive number");
+        return false;
+    }
+}
+
+int GetPow() // Реализация функции возведения в степень
+{
+    int pow = 1;
+    if(IsValidateNumber(num2)) // Вызов функции проверки наличия негативного числа
+    {
+        for (int i = 1; i <= num2; i++)
+        {
+            pow *= num1;
+        }
+    }
+    else
+    {
+        Environment.Exit(0); // В случае провала программа останавливается
+    }
+    
+    return pow;
+}
+
+Console.WriteLine(GetPow());
